d8b1a4bab9029203f58a0aeffff68c2e31f7d156
juanotravez <juan2@esotroejemplo.es>
2021-05-17 18:27:58 +0200

    Commit para el mensaje dentro de principal

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -5,7 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 	System.out.println("Hola mundo. Java Git.");
 	System.out.println("Esto es otra prueba del repositorio");
-
+	System.out.println("Pruebas Git. add cambio1");
 	}
 
 }
